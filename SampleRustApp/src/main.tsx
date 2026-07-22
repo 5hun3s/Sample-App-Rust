0197a383205732e362ae0d0e85a3cde7e5e02ff8
5hun3s <shunsaku4439@gmail.com>
2026-07-22 12:36:09 +0900

    restore

--- SampleRustApp/src/main.tsx
+++ SampleRustApp/src/main.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import ActiveWindowMonitor from "./components/activeWindowMonitor";
-
+// import App from "./App";
+// import ActiveWindowMonitor from "./components/activeWindowMonitor";
+import ActivityPage  from "./components/activityPage";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App/>
-    <ActiveWindowMonitor/>
+    <ActivityPage/>
   </React.StrictMode>,
 );
